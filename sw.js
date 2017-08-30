e43719b9ab154c1a6a5132977ef3245b9976fcbe
Alex Bezek <alex.bezek@cerner.com>
2017-08-29 21:34:26 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/blog/app-f2d62a05f6b5b6ad61ef.js","2f0698007faa9da45284bf44b058400d"],["/blog/commons-f1873a4f7ee992bae8f1.js","901a09c8f7992630fab52accc99a5aca"],["/blog/index.html","4dd7690e3c11ec78cd41f81ef84b0ddb"],["/blog/manifest.json","645dced826ac3b54454d6b110a21c80e"],["/blog/offline-plugin-app-shell-fallback/index.html","a665c5c41ada6813b0f72ec5003b052c"]];
+var precacheConfig = [["/blog/app-f2d62a05f6b5b6ad61ef.js","2f0698007faa9da45284bf44b058400d"],["/blog/commons-f1873a4f7ee992bae8f1.js","901a09c8f7992630fab52accc99a5aca"],["/blog/index.html","5943bf1f80f50ccf7976bf591e84c79e"],["/blog/manifest.json","645dced826ac3b54454d6b110a21c80e"],["/blog/offline-plugin-app-shell-fallback/index.html","974f3616259750782bf0fd78883f13c8"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
